fe68ac1e7e578a48274c483cdb5550053f181dc7
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-03-20 20:52:15 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        String id = scanner.nextLine();
-        System.out.println(id+"??!");
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String inputValues = bf.readLine();
+        System.out.println(inputValues+"??!");
     }
 }

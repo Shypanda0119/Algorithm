4ff2ae3342315ef6cd6828a4edae73d53795525b
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-03-20 21:03:04 +0900

    [Bronze III] Title: 곱셈, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,13 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+    public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      String str1 = br.readLine();
+      String str2 = br.readLine();
+      int a = Integer.parseInt(str1);
+      int b = Integer.parseInt(str2);
+      int c = b;
+      System.out.println(a * (c % 10));
+      c = c/10;
+      System.out.println(a * (c % 10));
+      c = c/10;
+      System.out.println(a * c);
+      System.out.println(a * b);
 
-public class Main{
-    public static void main(String args[]){
-        Scanner scanner = new Scanner(System.in);
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
-        System.out.println(A*((B%100)%10));
-        System.out.println(A*((B%100)/10));
-        System.out.println(A*(B/100));
-        System.out.println(A*B);
     }
 }

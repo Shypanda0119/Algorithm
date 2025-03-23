93bcaaa610bb7ec91ed637f26b9f63c74d66aff9
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-03-23 18:27:30 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,14 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        for(int i=1; i<=num; i++){
-            for(int j=0; j < i; j++){
-                System.out.print("*");
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+        for(int i = 0; i < num; i++){
+            for(int j = 0; j <= i ; j++) {
+                    bw.write('*');
+                }
+                bw.write("\n");
             }
-            System.out.println();
-        }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }

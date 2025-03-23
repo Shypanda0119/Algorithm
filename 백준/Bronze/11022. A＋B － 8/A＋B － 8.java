ebd4bea2bcd780669e3f99ea6527c18542a8a384
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-03-23 18:13:33 +0900

    [Bronze V] Title: A+B - 8, Time: 144 ms, Memory: 16400 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -1,28 +1,19 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
- 
-public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());    
-        int num1, num2;
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine()," ");
-            num1 = Integer.parseInt(st.nextToken());
-            num2 = Integer.parseInt(st.nextToken());
-			bw.write("Case #" + (i + 1) + ": " + num1 + " + " + num2 + " = "+ (num1 + num2)+ "\n");
-		}
-		br.close();
-		bw.flush();
-		bw.close();
- 
-	}
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+        for(int i = 0; i < num; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write("Case #" + (i+1) + ": " + a + " + " + b + " = " + (a+b) + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }

7823ac34c72b36065f045adeb8818bcfcbbb7542
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-03-23 18:42:18 +0900

    [Bronze V] Title: A+B - 5, Time: 116 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,30 +1,21 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
- 
-public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int num1, num2;
-		while(true){
-			st = new StringTokenizer(br.readLine()," ");
-            num1 = Integer.parseInt(st.nextToken());
-            num2 = Integer.parseInt(st.nextToken());
-            if(num1 == 0 && num2 == 0){
-                break;
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if( a == 0 && b == 0) break;
+            else{
+                bw.write((a+b) + "\n");
             }
-            bw.write((num1 + num2)+"\n");
-		}
-		br.close();
-		bw.flush();
-		bw.close();
- 
-	}
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }

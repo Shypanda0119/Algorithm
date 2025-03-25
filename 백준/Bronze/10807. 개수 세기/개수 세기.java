28fed046480b9e71ef0bdd7f2962fd95511ec9ef
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-03-25 12:51:32 +0900

    [Bronze V] Title: 개수 세기, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,34 +1,20 @@
-import java.io.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-        int count = 0;
-        int[] arr = new int[N];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());;
-
-        for(int i = 0; i < N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int b = Integer.parseInt(br.readLine());
-
-        for(int j = 0; j < arr.length; j++){
-            if(b == arr[j]) {
-                count++;
-            }
-        }
-        System.out.println(count);
-
-        br.close();
-        bw.flush();
-        bw.close();
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int num = Integer.parseInt(br.readLine());
+      int[] num_List = new int[num];
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int find_Num = Integer.parseInt(br.readLine());
+      int cnt = 0;
+      for(int i = 0; i < num; i++){
+          num_List[i] = Integer.parseInt(st.nextToken());
+          if (num_List[i] == find_Num) cnt++;
+      }
+      System.out.println(cnt);
     }
 }

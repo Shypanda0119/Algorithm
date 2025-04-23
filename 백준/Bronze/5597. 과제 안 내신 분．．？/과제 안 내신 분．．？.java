3767930032303509192f423b71ba06ed60181616
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-04-23 19:06:18 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 104 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean[] isPresent = new boolean[31];
+        for (int i=0; i<28; i++) {
+            int num = Integer.parseInt(br.readLine());
+            isPresent[num] = true;
+        }
+        for (int i=1; i<31; i++){
+            if(isPresent[i] != true){
+                System.out.println(i);
+            }
+        }
+    }
+}

da9c302f374fba6f86a6b2750cd321d28b96da66
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-04-24 09:45:54 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 116 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] alphabet = new int[26];
+        for(int i = 0; i < 26; i++) {
+            alphabet[i] = -1;
+        }
+        for(int i = 0; i < str.length(); i++) {
+            int index = str.charAt(i)-'a';
+            if(alphabet[index] == -1) {
+                alphabet[index] = i;
+            }
+        }
+        for(int i : alphabet) {
+            System.out.print(i+" ");
+        }
+    }
+}

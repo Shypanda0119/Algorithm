00e371fa56336928dab9af22ad06d1e2425f9ede
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-04-24 11:23:53 +0900

    [Bronze II] Title: 단어의 개수, Time: 272 ms, Memory: 52920 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        if (str.isBlank()) {
+            System.out.println("0");
+            return;
+        }
+        String[] strArr = str.trim().split(" ");
+        System.out.println(strArr.length);
+    }
+}

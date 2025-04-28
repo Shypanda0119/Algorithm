0ca0df50efae870d16b0dd708351cdee9233b655
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-04-28 19:34:13 +0900

    [level 0] Title: 접미사 배열, Time: 0.43 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 
 class Solution {
-    public String[] solution(String my_string) {
-        String[] suffixes = new String[my_string.length()];
-        for (int i = 0; i < my_string.length(); i++) {
-            suffixes[i] = my_string.substring(i);
-        }
-        Arrays.sort(suffixes);
-        return suffixes;
+    public String[] solution(String my_string){
+    String[] answer = new String[my_string.length()];
+    for(int i=0; i<answer.length; i++){
+        answer[i] = my_string.substring(i, answer.length);
+    }
+    Arrays.sort(answer);
+    return answer;
     }
 }

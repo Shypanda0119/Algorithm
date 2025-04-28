71856a11df5a42d18fb21372e56a1d2a85f99013
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-04-28 19:13:55 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.49 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        for(int i=0; i<arr.length; i++){
+            if(idx<=i && arr[i]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}

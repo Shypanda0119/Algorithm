e5c6a2ce43b2d4b170cd613a8c8dfa3db2df4e1f
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-04-29 19:27:49 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] croAlpha = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        String input = br.readLine();
+        for(String pattern : croAlpha){
+            input = input.replace(pattern, "*");
+        }
+
+        System.out.println(input.length());
+    }
+}

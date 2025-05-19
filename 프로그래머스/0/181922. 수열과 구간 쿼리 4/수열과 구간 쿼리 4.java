d9d660fe3017cf79973de8de06eaecd1e3fc9d23
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-05-19 18:06:26 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 3.66 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int i=0; i<queries.length; i++){
+            if (queries[i][2] == 0) continue;
+            for(int j=queries[i][0]; j<=queries[i][1]; j++){
+                if(j % queries[i][2] == 0) arr[j]++;
+            }
+        }
+        return arr;
+    }
+}

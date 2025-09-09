afdba9c6a5691eb876399c66c8cd61e59c28fdbd
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-09-09 20:48:42 +0900

    [level 1] Title: 폰켓몬, Time: 6.04 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+
+        for (int num : nums) {
+            numSet.add(num);
+        }
+        return Math.min(nums.length/2, numSet.size());
+    }
+}

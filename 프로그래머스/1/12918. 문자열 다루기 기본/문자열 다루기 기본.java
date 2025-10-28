86660b84f2b03d7b63631b064395b1a99023ef05
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-10-28 12:03:10 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(String s) {
+        if(s.length() == 4 || s.length() == 6){
+            for(int i=0; i<s.length(); i++){
+                if(s.charAt(i) >= 'A' && s.charAt(i) <= 'z') return false;
+            }
+            return true;
+        }
+        return false;
+    }
+}

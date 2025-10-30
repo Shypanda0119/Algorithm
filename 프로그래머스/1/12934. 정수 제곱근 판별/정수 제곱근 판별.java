971f3f11004d128e7d9e858e12af66c90e89f173
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2025-10-30 13:01:43 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.10 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(long n) {
+        double sqrt = Math.sqrt(n);
+        if (sqrt == (long) sqrt) {
+            long x = (long) sqrt + 1;
+            return x * x;
+        }
+        return - 1;
+    }
+}

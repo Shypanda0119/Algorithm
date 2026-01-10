a25fc8d30aae67b7bbdcf3dc0c2f5337e7845028
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2026-01-10 22:33:27 +0900

    [level 0] Title: 평행, Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[][] dots) {
+        if (isParallel(dots[0], dots[1], dots[2], dots[3])) return 1;
+        if (isParallel(dots[0], dots[2], dots[1], dots[3])) return 1;
+        if (isParallel(dots[0], dots[3], dots[1], dots[2])) return 1;
+        return 0;
+    }
+
+    private boolean isParallel(int[] a, int[] b, int[] c, int[] d) {
+        return (a[1] - b[1]) * (c[0] - d[0]) == (c[1] - d[1]) * (a[0] - b[0]);
+    }
+}

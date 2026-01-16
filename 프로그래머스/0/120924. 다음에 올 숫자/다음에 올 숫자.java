b4e2bbacc28f5fb3ab561f5fde3896c906defba3
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2026-01-16 17:28:07 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] common) {
+        int n = common.length;
+        if (common[1] - common[0] == common[2] - common[1]) {
+            return common[n - 1] + (common[1] - common[0]);
+        }
+        return common[n - 1] * (common[1] / common[0]);
+    }
+}

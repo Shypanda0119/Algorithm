38418a481fcbeeb77a55d839c20494b10d2ff789
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2026-01-27 18:29:25 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[][] paper = new int[100][100];
+    int N = Integer.parseInt(br.readLine());
+    int extent = 0;
+      for(int i=0; i<N; i++){
+        StringTokenizer st =  new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        for(int j=x; j<x+10; j++){
+          for(int k=y; k<y+10; k++){
+            paper[j][k] = 1;
+          }
+        }
+      }
+
+      for(int i=0; i<100; i++){
+        for(int j=0; j<100; j++){
+          if(paper[i][j] == 1) extent++;
+        }
+      }
+      System.out.println(extent);
+    }
+  }

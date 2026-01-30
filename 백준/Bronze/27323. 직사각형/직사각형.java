070477ba7be0570a0429407e50c8914889488830
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2026-01-30 17:49:33 +0900

    [Bronze V] Title: 직사각형, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int width = Integer.parseInt(br.readLine());
+        int height = Integer.parseInt(br.readLine());
+        System.out.print(width * height);
+    }
+}

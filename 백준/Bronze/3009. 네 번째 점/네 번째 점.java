d04021b99d4bca40db7df76bc58cba6c2376aa51
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2026-02-01 23:46:21 +0900

    [Bronze III] Title: 네 번째 점, Time: 120 ms, Memory: 15980 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] x = new int[3];
+        int[] y = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ansX = x[0] ^ x[1] ^ x[2];
+        int ansY = y[0] ^ y[1] ^ y[2];
+
+        System.out.println(ansX + " " + ansY);
+    }
+}

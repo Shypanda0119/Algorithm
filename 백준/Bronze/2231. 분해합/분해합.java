5e09c5bbfe2ec6dd1b0df983e75e9c8b359063be
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2026-02-14 22:44:16 +0900

    [Bronze II] Title: 분해합, Time: 168 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        int length = String.valueOf(N).length();
+        int start = Math.max(1, N - 9 * length);
+        
+        int answer = 0;
+        
+        for (int i = start; i < N; i++) {
+            int sum = i;
+            int temp = i;
+            
+            while (temp > 0) {
+                sum += temp % 10;  
+                temp /= 10;
+            }
+            
+            if (sum == N) {
+                answer = i;
+                break; 
+            }
+        }
+        
+        System.out.println(answer);
+    }
+}

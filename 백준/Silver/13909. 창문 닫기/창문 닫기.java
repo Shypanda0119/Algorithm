1aeb00ed320c77886313633011005add7b034989
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2026-03-19 16:13:22 +0900

    [Silver V] Title: 창문 닫기, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    long n = Long.parseLong(br.readLine());
+    System.out.println((long)Math.sqrt(n));
+  }
+}

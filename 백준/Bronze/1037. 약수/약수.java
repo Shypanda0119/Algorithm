313b29f08a68328682b0c1e44ee5931e83c8a327
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2026-03-21 20:58:41 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+        
+        int result = arr[0] * arr[n - 1];
+        
+        System.out.println(result);
+    }
+}

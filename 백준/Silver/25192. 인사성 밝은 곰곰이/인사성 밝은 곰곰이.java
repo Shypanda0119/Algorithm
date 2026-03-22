8cb549af60c7ea4e40c744fa48606081608b2a61
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2026-03-22 23:33:45 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 252 ms, Memory: 25452 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        
+        int count = 0;
+        
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            
+            if (input.equals("ENTER")) {
+                set.clear(); // 새로운 채팅방
+            } else {
+                if (!set.contains(input)) {
+                    set.add(input);
+                    count++;
+                }
+            }
+        }
+        
+        System.out.println(count);
+    }
+}

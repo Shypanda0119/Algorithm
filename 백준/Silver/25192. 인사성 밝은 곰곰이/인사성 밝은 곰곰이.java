e4a11328a89e09997b09e69ad773a6e6b9f3aae7
Shypanda0119 <127657297+Shypanda0119@users.noreply.github.com>
2026-03-30 14:36:17 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 244 ms, Memory: 25456 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -2,27 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        Set<String> set = new HashSet<>();
-        
-        int count = 0;
-        
-        for (int i = 0; i < N; i++) {
-            String input = br.readLine();
-            
-            if (input.equals("ENTER")) {
-                set.clear(); // 새로운 채팅방
-            } else {
-                if (!set.contains(input)) {
-                    set.add(input);
-                    count++;
-                }
-            }
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+
+    Set<String> set = new HashSet<>();
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+      String str = br.readLine();
+
+      if (str.equals("ENTER")) {
+        set.clear();
+      } else {
+        if (!set.contains(str)) {
+          set.add(str);
+          count++;
         }
-        
-        System.out.println(count);
+      }
     }
+
+    System.out.println(count);
+  }
 }
